bbccb5dede80ea89f7d097bde335af9cc77f9a6e
Liedev <lieven_janssens@hotmail.be>
2020-06-15 22:05:43 +0200

    Mainwindow aangemaakt, werkt niet. Verder gegaan naar event bewerken

--- WPF_Project/Artmin/MainWindow.xaml.cs
+++ WPF_Project/Artmin/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Artmin_DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace Artmin
 {
@@ -27,7 +29,9 @@ namespace Artmin
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
+            icItemcontrolEvents.ItemsSource = DatabaseOperations.OphalenEvents();
+            EventBewerken eventBewerken = new EventBewerken();
+            eventBewerken.Show();
         }
     }
 }
--- WPF_Project/Artmin_DAL/DatabaseOperations.cs
+++ WPF_Project/Artmin_DAL/DatabaseOperations.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Runtime.CompilerServices;
 
 namespace Artmin_DAL
 {
     public static class DatabaseOperations
     {
-
+        public static List<Event> OphalenEvents()
+        {
+            using (ArtminEntities artminEntities = new ArtminEntities())
+            {
+                return artminEntities.Event
+                    .Include(x => x.Eventtype.Naam)
+                    .Include(x => x.Datum)
+                    .Include(x => x.Startuur)
+                    .Include(x => x.Einduur)
+                    .ToList();           
+            }
+        }
     }
 }
